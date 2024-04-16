f95f876c380a64426a213511e4431e65db1392c3
Giorgi Mikautadze <giorgi.mikautadze2223@gmail.com>
2024-04-16 22:39:50 +0400

    feat:added migrations and factories

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,42 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
+    use HasFactory;
+    protected $table = 'jobs_listings';
 
-            [
-                'id' => 1,
-                'title' => 'Programmer',
-                'salary' => '120,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Doctor',
-                'salary' => '500,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '60,000'
-            ],
-
-        ];
-    }
-
-    public static function find(int $id):array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if(!$job){
-            abort(404);
-        }
-
-        return $job;
-    }
-
+    protected $fillable = ['title', 'salary'];
 }
